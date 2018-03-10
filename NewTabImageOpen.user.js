3ec359a8da8f0c80b134107357c00e62638b3369
NightLancer <NightLancer@users.noreply.github.com>
2018-03-10 17:03:57 +0200

    Update NewTabImageOpen.user.js

--- NewTabImageOpen.user.js
+++ NewTabImageOpen.user.js
@@ -4,7 +4,7 @@
 // @description     additional script for "Pixiv Arts Preview & Followed Atrists Coloring" for propper new tab original images opening
 // @author          NightLancerX
 // @match           https://i.pximg.net/img-original/*
-// @version         0.21
+// @version         0.22
 // @downloadURL     https://github.com/NightLancer/PixivPreview/raw/master/NewTabImageOpen.user.js
 // @grant           none
 // ==/UserScript==
@@ -12,9 +12,7 @@
 (function ()
 {
   document.addEventListener("DOMContentLoaded", function()
-  {
-    var url = document.URL,
-        s = url.slice(-3);    
+  { 
     if (document.body.innerText.substring(0,3)==='404') 
     {
       window.close();
