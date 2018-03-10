664997ad76b2d70147ce0c995ae5d81287ce45e1
NightLancer <NightLancer@users.noreply.github.com>
2018-03-10 17:59:16 +0200

    Update NewTabImageOpen.user.js
    changed loading identification method

--- NewTabImageOpen.user.js
+++ NewTabImageOpen.user.js
@@ -4,18 +4,19 @@
 // @description     additional script for "Pixiv Arts Preview & Followed Atrists Coloring" for propper new tab original images opening
 // @author          NightLancerX
 // @match           https://i.pximg.net/img-original/*
-// @version         0.22
+// @version         0.23
+// @homepageURL     https://github.com/NightLancer/PixivPreview
 // @downloadURL     https://github.com/NightLancer/PixivPreview/raw/master/NewTabImageOpen.user.js
 // @grant           none
 // ==/UserScript==
 
 (function ()
 {
-  document.addEventListener("DOMContentLoaded", function()
-  { 
+  window.onload = function()
+  {
     if (document.body.innerText.substring(0,3)==='404') 
     {
       window.close();
     }
-  });  
+  );
 }) ();
