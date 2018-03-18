a26ffde30d776cd65cc470a1f51513fc575ee650
NightLancer <NightLancer@users.noreply.github.com>
2018-03-18 19:39:33 +0200

    added russian description

--- NewTabImageOpen.user.js
+++ NewTabImageOpen.user.js
@@ -1,10 +1,12 @@
 // ==UserScript==
 // @name            NewTabImageOpen
+// @name:ru         NewTabImageOpen
 // @namespace       Pixiv
 // @description     additional script for "Pixiv Arts Preview & Followed Atrists Coloring" for propper new tab original images opening
+// @description:ru  дополнительный скрипт для «Pixiv Arts Preview & Followed Atrists Coloring» для правильной обработки открытия артов в новой вкладке
 // @author          NightLancerX
 // @match           https://i.pximg.net/img-original/*
-// @version         0.23
+// @version         0.24
 // @homepageURL     https://github.com/NightLancer/PixivPreview
 // @downloadURL     https://github.com/NightLancer/PixivPreview/raw/master/NewTabImageOpen.user.js
 // @grant           none
