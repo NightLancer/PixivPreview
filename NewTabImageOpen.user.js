f0591c10d11cb1dd8eb826974dc3b1ed01ae2bdd
NightLancer <NightLancer@users.noreply.github.com>
2018-04-13 22:59:14 +0300

    Update NewTabImageOpen.user.js
    fixed bug, when page fully loads before script started, so it never runs

--- NewTabImageOpen.user.js
+++ NewTabImageOpen.user.js
@@ -2,11 +2,11 @@
 // @name            NewTabImageOpen
 // @name:ru         NewTabImageOpen
 // @namespace       Pixiv
-// @description     additional script for "Pixiv Arts Preview & Followed Atrists Coloring" for propper new tab original images opening
+// @description     additional script for "Pixiv Arts Preview & Followed Atrists Coloring" for proper new tab original images opening
 // @description:ru  дополнительный скрипт для «Pixiv Arts Preview & Followed Atrists Coloring» для правильной обработки открытия артов в новой вкладке
 // @author          NightLancerX
 // @match           https://i.pximg.net/img-original/*
-// @version         0.24
+// @version         0.25
 // @homepageURL     https://github.com/NightLancer/PixivPreview
 // @downloadURL     https://github.com/NightLancer/PixivPreview/raw/master/NewTabImageOpen.user.js
 // @grant           none
@@ -14,11 +14,13 @@
 
 (function ()
 {
-  window.onload = function()
-  {
-    if (document.body.innerText.substring(0,3)==='404') 
+    function check()
     {
-      window.close();
+        if (document.body.innerText.substring(0,3)==='404')
+        {
+            window.close();
+        }
     }
-  );
+    check();
+    document.addEventListener("DOMContentLoaded", check());
 }) ();
