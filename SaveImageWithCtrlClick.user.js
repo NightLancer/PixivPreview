e91a9d9dc5387ec64bff7d159fb366ef541ad06d
NightLancer <NightLancer@users.noreply.github.com>
2018-06-15 00:41:12 +0300

    increased default timeout for closing tab
    increased default timeout for closing tab to 1s(due to new Chrome ver. bug) and made a TIMEOUT variable to manual changing it if needed

--- SaveImageWithCtrlClick.user.js
+++ SaveImageWithCtrlClick.user.js
@@ -2,18 +2,22 @@
 // @name            Saving Pixiv Images with Ctrl+Click
 // @name:ru         Saving Pixiv Images with Ctrl+Click
 // @namespace       Pixiv
-// @description     Additional script to «Pixiv Arts Preview & Followed Atrists Coloring» for saving images with Ctrl+Click on pixiv.
-// @description:ru  Дополнительный скрипт для «Pixiv Arts Preview & Followed Atrists Coloring» для сохранения изображений с помощью Ctrl+Click на сайте pixiv.
+// @description     Additional script to «Pixiv Arts Preview & Followed Atrists Coloring» for saving images with Ctrl+Click on pixiv. If you've opened original from other page, you can save it here too, by pressing the same Ctrl+Click combination.
+// @description:ru  Дополнительный скрипт для «Pixiv Arts Preview & Followed Atrists Coloring» для сохранения изображений с помощью Ctrl+Click на сайте pixiv. Если вы открыли оригинал с другой страницы, вы также можете сохранить его здесь, нажав Ctrl + Click.
 // @author          NightLancerX
 // @match           https://i.pximg.net/img-original/*
-// @version         1.0
+// @version         1.11
 // @homepageURL     https://github.com/NightLancer/PixivPreview
 // @downloadURL     https://github.com/NightLancer/PixivPreview/raw/master/SaveImageWithCtrlClick.user.js
+// @license         MIT License
 // @grant           none
 // ==/UserScript==
 
 (function() {
     'use strict';
+    let TIMEOUT = 1000; //you may change it from ~500 or less, while downloading is still works and you want to wait less
+                        //or increase it over 1000 until downloading starts to work.
+
     if (document.URL.slice(-3)=="s=1")
     {
         let anchor = document.createElement('a'),
@@ -30,10 +34,27 @@
             document.body.appendChild(anchor);
             anchor.click();
             done = true;
-            let timerId = setTimeout(function(){window.close()}, 500);
+            let timerId = setTimeout(function(){window.close()}, TIMEOUT);
         };
 
         document.addEventListener("DOMContentLoaded", save());
         save(); //just in case of missing DOMContentLoaded event(if script hasn't loaded in time for some reasons)
+    }
+    else //saving with ctrl+click on current page
+    {
+        let img = document.querySelectorAll('img')[0];
+        img.onclick = function(e)
+        {
+            if (e.ctrlKey)
+            {
+                e.preventDefault();
+                let anchor = document.createElement('a');
+                anchor.href = img.src;
+                anchor.target = '_self';
+                anchor.download = img.src.substring(img.src.lastIndexOf("/")+1);
+                document.body.appendChild(anchor);
+                anchor.click();
+            };
+        };
     }
 })();
